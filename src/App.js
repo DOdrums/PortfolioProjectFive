0034a4a7d74dfb6cd05079dfc440cab453d11632
DOdrums <dirkornee@hotmail.com>
2023-01-23 17:14:25 +0100

    Add temporary component to test local connection to backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import Data from "./component/Data";
 
 function App() {
   return (
     <div className="App">
       <Button variant="primary">Primary</Button>
+      <Data></Data>
     </div>
   );
 }
---
+++ src/component/Data.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+
+const Data = () => {
+  const [posts, setPosts] = useState({ results: [] });
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/`);
+        setPosts(data);
+      } catch (err) {}
+    };
+    fetchPosts();
+  }, []);
+
+  return <div>heyyyy</div>;
+};
+
+export default Data;
