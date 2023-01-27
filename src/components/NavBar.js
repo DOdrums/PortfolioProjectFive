6564c29157779e9e5679c994ccace68f23a35c09
DOdrums <dirkornee@hotmail.com>
2023-01-27 15:31:36 +0100

    Multiple changes: - add styling to NavBar - add react-router-dom package - add routing to app.js for signup, homepage and signin - add router as parent in index.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,42 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo-no-background.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-      <Navbar.Brand>
-        <img src={logo} alt="logo" height="43" />
-      </Navbar.Brand>
+      <NavLink to="/">
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="43" />
+        </Navbar.Brand>
+      </NavLink>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link>
-            <i className="fas fa-home"></i> Wall
-          </Nav.Link>
-          <Nav.Link>
-            <i className="fas fa-sign-in-alt"></i> Sign In
-          </Nav.Link>
-          <Nav.Link>
+          <NavLink
+            exact
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/"
+          >
+            <i className="fa-solid fa-icons"></i> Wall
+          </NavLink>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/signin"
+          >
+            <i className="fa-solid fa-arrow-right-to-bracket"></i> Sign In
+          </NavLink>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/signup"
+          >
             <i className="fas fa-user-plus"></i> Sign Up
-          </Nav.Link>
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
