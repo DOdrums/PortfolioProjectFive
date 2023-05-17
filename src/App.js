b814ace30913420110fbe666163ffe2a237e941e
DOdrums <dirkornee@hotmail.com>
2023-05-17 10:30:51 +0000

    Add SongCreateForm to upload songs and add url to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import SongCreateForm from "./pages/songs/SongCreateForm";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/songs/create" render={() => <SongCreateForm />} />
         </Switch>
       </Container>
     </div>
