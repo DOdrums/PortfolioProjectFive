8f0409bed12e9e9d02bfc694e83f7116f9e08b2d
DOdrums <dirkornee@hotmail.com>
2023-05-18 11:16:16 +0000

    Setup basic template in PostPage.js to view a post

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import SongCreateForm from "./pages/songs/SongCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/songs/create" render={() => <SongCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
         </Switch>
       </Container>
     </div>
