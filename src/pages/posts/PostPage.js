2fb47212a203511d4e5b8e9f3a8d9d8f85db73f1
DOdrums <dirkornee@hotmail.com>
2023-05-29 13:09:48 +0000

    Add comment form and functionality to post comment: - Add CommentCreateForm to create post comments - Add CommentCreateFormSong to create song comments - Add both components to PostPage and SongPage respectively and pass props - Add CommentCreateEditForm.module.css to style comment form

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,28 +7,32 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Post from "./Post";
 
 function PostPage() {
-    const { id } = useParams();
-    const [post, setPost] = useState({ results: [] });
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_avatar = currentUser?.profile_avatar;
+  const [comments, setComments] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const [{ data: post }] = await Promise.all([
-                    axiosReq.get(`/posts/${id}`)
-                ])
-                setPost({ results: [post] })
-                console.log(post)
-            } catch (err) {
-                console.log(err)
-            }
-        };
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        handleMount();
-    }, [id]);
-
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
@@ -36,7 +40,17 @@ function PostPage() {
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.BorderBox}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_avatar}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
--- src/pages/songs/SongPage.js
+++ src/pages/songs/SongPage.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import CommentCreateForm from "../comments/CommentCreateFormSong";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -10,33 +12,45 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Song from "./Song";
 
 function SongPage() {
-    const { id } = useParams();
-    const [song, setSong] = useState({ results: [] });
+  const { id } = useParams();
+  const [song, setSong] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_avatar = currentUser?.profile_avatar;
+  const [comments, setComments] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const [{ data: song }] = await Promise.all([
-                    axiosReq.get(`/songs/${id}`)
-                ])
-                setSong({ results: [song] })
-                console.log(song)
-            } catch (err) {
-                console.log(err)
-            }
-        };
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: song }] = await Promise.all([
+          axiosReq.get(`/songs/${id}`),
+        ]);
+        setSong({ results: [song] });
+        console.log(song);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-        handleMount();
-    }, [id]);
-
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Song {...song.results[0]} setSongs={setSong} songPage/>
+        <Song {...song.results[0]} setSongs={setSong} songPage />
         <Container className={appStyles.BorderBox}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_avatar}
+              song={id}
+              setSong={setSong}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
